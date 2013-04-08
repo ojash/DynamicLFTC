604ee05e29bcac46747fdfde94ad6e1a6b34fcc0
ojash <ojash@live.com>
2013-04-08 09:25:19 +0545

    now this is a working copy

--- application/modules/cms/controllers/IndexController.php
+++ application/modules/cms/controllers/IndexController.php
@@ -1,20 +1,23 @@
 <?php
 
-class IndexController extends Zend_Controller_Action
+class Cms_IndexController extends Zend_Controller_Action
 {
 
 	public function init()
 	{
+            Zend_Layout::getMvcInstance()->setLayout('cms');
 	/* Initialize action controller here */
 	}
 
 	public function indexAction()
 	{
+            
 	// action body
 	}
         
         public function sampleAction(){
-            echo "this hand that";
+            //Zend_Layout::getMvcInstance()->setLayout('cms');
+            echo "<font color='red'>this hand that</font>";
         }
 
 }
--- application/modules/default/Bootstrap.php
+++ application/modules/default/Bootstrap.php
@@ -2,22 +2,22 @@
 
 class Default_Bootstrap extends Zend_Application_Module_Bootstrap
 {
-    protected function _initAutoload() {
-        $front = $this->bootstrap("frontController")->frontController;
-        $modules = $front->getControllerDirectory();
-        $default = $front->getDefaultModule();
-
-        foreach (array_keys($modules) as $module) {
-            if ($module === $default) {
-                continue;
-            }
-
-            $moduleloader = new Zend_Application_Module_Autoloader(array(
-                'namespace' => $module,
-                'basePath' => $front->getModuleDirectory($module)));
-            }
-            
-    }
+//    protected function _initAutoload() {
+//        $front = $this->bootstrap("frontController")->frontController;
+//        $modules = $front->getControllerDirectory();
+//        $default = $front->getDefaultModule();
+//
+//        foreach (array_keys($modules) as $module) {
+//            if ($module === $default) {
+//                continue;
+//            }
+//
+//            $moduleloader = new Zend_Application_Module_Autoloader(array(
+//                'namespace' => $module,
+//                'basePath' => $front->getModuleDirectory($module)));
+//            }
+//            
+//    }
 }
 
 ?>
